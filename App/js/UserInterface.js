d0a434c23a7b797156ee6a17b9b1246423d656e9
Kiaan Castillo <kiaancastillo@gmail.com>
2021-04-10 23:20:05 -0700

    Add double thumbs to date range slider; on slider change, also change the data being graphed and the date range displayed

--- App/js/UserInterface.js
+++ App/js/UserInterface.js
@@ -54,6 +54,14 @@ class UserInterface {
     this.region = newRegion;
   }
 
+  setStartDateDisplay(value) {
+    document.querySelector(".date-range-display.start-date").textContent = value
+  }
+
+  setEndDateDisplay(value) {
+    document.querySelector(".date-range-display.end-date").textContent = value
+  }
+
   /**
    * Capitalize the first letter of a string.
    * 
@@ -176,6 +184,47 @@ class UserInterface {
         maintainAspectRatio: false,
       },
     })
+  }
+
+  setDoubleSlider() {
+    const inputLeft = document.getElementById("slider-left")
+    const inputRight = document.getElementById("slider-right")
+
+    const thumbLeft = document.querySelector(".slider > .thumb.slider-left")
+    const thumbRight = document.querySelector(".slider > .thumb.slider-right")
+    const range = document.querySelector(".slider > .range")
+
+    const startDateDisplay = document.querySelector(".date-range-display.start-date")
+    const endDateDisplay = document.querySelector(".date-range-display.end-date")
+
+    const setLeftValue = () => {
+      const min = parseInt(inputLeft.min)
+      const max = parseInt(inputLeft.max)
+
+      inputLeft.value = Math.min(parseInt(inputLeft.value), parseInt(inputRight.value) - 1)
+
+      const percent = ((inputLeft.value - min) / (max - min)) * 100
+
+      thumbLeft.style.left = percent + "%"
+      range.style.left = percent + "%"
+    }
+    setLeftValue();
+
+    const setRightValue = () => {
+      const min = parseInt(inputRight.min)
+      const max = parseInt(inputRight.max)
+
+      inputRight.value = Math.max(parseInt(inputRight.value), parseInt(inputLeft.value) + 1)
+      
+      const percent = ((inputRight.value - min) / (max - min)) * 100
+      
+      thumbRight.style.right = (100 - percent) + "%"
+      range.style.right = (100 - percent) + "%"
+    }
+    setRightValue()
+
+    inputLeft.addEventListener("input", setLeftValue)
+    inputRight.addEventListener("input", setRightValue)
   }
 
   /**
--- App/js/start.js
+++ App/js/start.js
@@ -3,6 +3,8 @@ let ui = new UserInterface('2013-01-01', '2020-01-01', 'new york');
 async function setUserInterface() {
   await ui.setGraph();
   ui.setStats();
+  ui.setStartDateDisplay(ui.startDate)
+  ui.setEndDateDisplay(ui.endDate)
 }
 
 /**
@@ -10,12 +12,31 @@ async function setUserInterface() {
  */
 function onStart() {
   ui.setLocationDropdown();
+  ui.setDoubleSlider();
   setUserInterface();
 }
 
 function regionSelected(region) {
   ui.setRegion(region.toLowerCase());
   setUserInterface();
+}
+
+function getNewDate(value) {
+  const newDate = new Date('2013-01-01')
+  newDate.setDate(newDate.getDate() + parseInt(value))
+  return newDate
+}
+
+function startDateSelected(value) {
+  const newDate = getNewDate(value)
+  ui.setStartDate(newDate.toJSON().slice(0, 10))
+  setUserInterface()
+}
+
+function endDateSelected(value) {
+  const newDate = getNewDate(value)
+  ui.setEndDate(newDate.toJSON().slice(0, 10))
+  setUserInterface()
 }
 
 onStart()
